2f00f26cb0570e1387b12b6f11930004182aac4e
Robert McKenney <robert@mckenney.ca>
2018-06-01 11:48:26 -0400

    Completed Module4

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,14 +2,17 @@
   <div>
     <h1>{{ title }}</h1>
     <ul>
-      <li v-for="book in books">{{ book.title }}: {{ book.author }}</li>
+      <book-item v-for="book in books" :book="book" />
     </ul>
   </div>
 </template>
 
 <script>
+import BookItem from './BookItem';
+
 export default {
   name: 'BookList',
+  components: { BookItem },
   data: () => ({
     title: 'All Books',
     books: [
